8a2a2fa0b8100983d11fc84f4d7432a309aa41fa
Tim <timburanicz@gmail.com>
2023-02-22 00:01:25 -0600

    created profile.py

---
+++ profile.py
@@ -0,0 +1,8 @@
+class Profile:
+    name = " "
+    version = "1.0"
+    inputs = ["Composite", "SCART-CV", "S-Video", "YPbPr", "SCART-RGB"]
+    resolutions = ["1080p", "480p", "720p", "1440p"]
+
+    def __init__(self):
+        pass
